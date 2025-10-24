5aa45b2ee9ae30e8815f4bdcbd7d4677761830fa
Mariano Camposeco <mariano1941@outlook.es>
2025-10-23 18:54:20 -0600

    fix: raname dir feat: add pharmacy service route

--- api-gateway/src/main/java/com/sa/healntrack/api_gateway/SwaggerConfig.java
+++ api-gateway/src/main/java/com/sa/healntrack/api_gateway/SwaggerConfig.java
@@ -28,9 +28,9 @@ class SwaggerConfig {
                 locator.getRouteDefinitions().collectList().block();
         Set<AbstractSwaggerUiConfigProperties.SwaggerUrl> urls = new HashSet<>();
         definitions.stream()
-                .filter(routeDefinition -> routeDefinition.getId().matches(".*-microservice"))
+                .filter(routeDefinition -> routeDefinition.getId().matches(".*-service"))
                 .forEach(routeDefinition -> {
-                    String name = routeDefinition.getId().replaceAll("-microservice", "");
+                    String name = routeDefinition.getId().replaceAll("-service", "");
                     AbstractSwaggerUiConfigProperties.SwaggerUrl swaggerUrl =
                             new AbstractSwaggerUiConfigProperties.SwaggerUrl(
                                     name, DEFAULT_API_DOCS_URL + "/" + name, null);
